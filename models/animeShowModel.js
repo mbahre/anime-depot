f12be7e1ec2bb094cff16abef9d5dde7bd9d84dd
Michael B <michaelbahreini@gmail.com>
2022-06-04 09:20:33 -0500

    added model

---
+++ models/animeShowModel.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const animeShowSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  certificate: {
+    type: String,
+  },
+  year: {
+    type: String,
+  },
+  stars: {
+    type: [String],
+  },
+  genre: {
+    type: [String],
+  },
+  rating: {
+    type: Number,
+  },
+  summary: {
+    type: String,
+  },
+  storyline: {
+    type: String,
+  },
+});
+
+const AnimeShow = new mongoose.model("AnimeShow", animeShowSchema);
