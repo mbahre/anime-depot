2a72c03141346af9d30a3d7ce6c828afc7856022
Michael B <michaelbahreini@gmail.com>
2022-06-05 09:22:20 -0500

    connected db

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
 const app = require("./app");
+const mongoose = require("mongoose");
+
+const DB = async function () {
+  try {
+    await mongoose.connect(process.env.DATABASE_LOCAL);
+    console.log("Connected to the DB successfully!");
+  } catch (err) {
+    console.log(err);
+  }
+};
+DB();
 
 const port = process.env.PORT;
 app.listen(port, function () {
