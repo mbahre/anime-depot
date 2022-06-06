618ef153bcde127e8c34732d577e50b208ddd170
Michael B <michaelbahreini@gmail.com>
2022-06-05 20:08:15 -0500

    added middleware

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 require("dotenv").config({ path: "./config.env" });
 const express = require("express");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 
 const app = express();
+
+const limiter = rateLimit({
+  windowMs: 60 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+app.use(limiter);
+
+app.use(helmet());
+
+app.use(mongoSanitize());
+
+app.use(xss());
 
 app.get("/api/animeshows", function (req, res) {
   res
